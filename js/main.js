cc03acc3040331a29a3e41caf4d146b2f1e2331d
freitassp <priettosamuel@gmail.com>
2021-03-09 07:33:34 -0300

    onmouseover e out

--- js/main.js
+++ js/main.js
@@ -6,3 +6,29 @@ console.log(lista);*/
 function clicou(){
    document.getElementById("agradecimento").innerHTML = "Obrigado por clicar"; 
 }
+
+function redirecionar(){
+   window.open("https://www.google.com.br");//outra aba
+   window.location.href = "https://www.google.com.br"//mesma janela
+
+}
+
+function trocar(elemento){
+   //alert("Trocar texto");
+   //document.getElementById("mousemove").innerHTML = "Troquei texto";
+   elemento.innerHTML = "Troquei texto";
+   
+}
+
+function voltar(elemento){
+   //document.getElementById("mousemove").innerHTML = "Troquei texto";
+   elemento.innerHTML = "Passe mouse aqui"
+}
+
+function load(){
+      alert("pagina carregada");
+}
+
+function funcaoChange(elemento){
+  console.log(elemento.value);
+}
